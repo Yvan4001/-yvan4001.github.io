03b080ad29e59ab6a3b2249b93aa3ddf399eeaaf
Yvan Simon <114400983+Yvan4001@users.noreply.github.com>
2022-11-04 10:40:30 +0100

    Modification du script.js + recup de l'html

---
+++ script.js
@@ -0,0 +1,43 @@
+let items = document.querySelectorAll('#tier-row');
+
+let dragItem;
+let lastDragItem;
+let tempDragItem = document.createElement('div');
+document.querySelector('.tier-list').append(tempDragItem);
+tempDragItem.setAttribute('draggable', true);
+
+items.forEach(item =>
+{
+    item.addEventListener('dragstart', dragStart(e));
+    item.addEventListener('dragend', dragEnd(e));
+    item.addEventListener('dragover', dragOver(e));
+    item.addEventListener('dragleave', dragLeave(e));
+});
+
+function dragStart(e)
+{ 
+    dragItem = e.target;
+    item.parentElement.appendChild(tempDragItem);
+}
+
+function dragEnd(e)
+{ 
+    if (lastDragItem != dragItem)
+    { 
+        dragItem.remove();
+        lastDragItem.before(dragItem);
+        tempDragItem.remove();
+    }
+}
+
+function dragOver(e)
+{
+    lastDragItem = e.target;
+    e.target.style.border = '1px solid red';
+}
+
+
+function dragLeave(e)
+{ 
+    e.target.style.border = '1px solid black';
+}
